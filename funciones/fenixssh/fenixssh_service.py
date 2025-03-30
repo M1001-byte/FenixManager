f08dcc4f71f0ed971b6aee1828a5d8ff90ee76eb
M1001-Byte <mathiue11@protonmail.com>
2025-03-30 16:31:54 -0300

    Update fenixssh and added support for d port forwarding, ( socks5 )

--- funciones/fenixssh/fenixssh_service.py
+++ funciones/fenixssh/fenixssh_service.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-import json, subprocess, os,socket, time, signal, sys
-
-def handle_signal(signal_number, frame):
-    """ Manejador de señales para detectar detención o reinicio """
-    if signal_number == signal.SIGTERM:
-        os.remove('/var/log/FenixManager/connFenixssh.json')
-    elif signal_number == signal.SIGHUP:
-        os.remove('/var/log/FenixManager/connFenixssh.json')
-    sys.exit(0)  # Salir con éxito
-
-# Registrar las señales que deseas manejar
-signal.signal(signal.SIGTERM, handle_signal)  # Manejar la señal de terminación
-signal.signal(signal.SIGHUP, handle_signal)   # Manejar la señal de reinicio
-
+import json, subprocess, os,socket, time
 
 def port_is_open(port:int) -> int:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
